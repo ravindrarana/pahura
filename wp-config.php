5aa172096b7257ec1b141521aebad3530c2f2a73
ravindrarana <projectsaathi@gmail.com>
2020-03-22 12:40:38 +0545

    set database password

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'pahura' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'whoami' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
